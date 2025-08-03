574da6fa6b23f1556c3f23c97dfdbc8ab549bc19
ASMaa-AHMED146 <158354808+ASMaa-AHMED146@users.noreply.github.com>
2025-08-03 23:24:05 +0300

    pagination

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
 import { Button } from "@/components/ui/button";
+import Search from "./Components/ui/Search/Search"
 
 function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>app</Button>
+      <Search/>
     </div>
   );
 }
