7316b4bad94c4058fa3b34d522d9ca4b05f23285
Ibrahim Abdallah <hemade@gmail.com>
2025-08-04 17:39:42 +0300

    #

--- client/src/Layouts/MainLayouts.jsx
+++ client/src/Layouts/MainLayouts.jsx
@@ -4,6 +4,13 @@ import { Outlet } from "react-router-dom";
 const MainLayouts = () => {
   return (
     <>
+      <Header />
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Search />
+
+        <Button>app</Button>
+        <DemoPage></DemoPage>
+      </div>
       <Outlet />
     </>
   );
